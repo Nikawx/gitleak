3a309bc6906b642e67be8c4d29a427f0aaf11822
victor <victor.myn24@gmail.com>
2025-05-19 12:32:26 +0200

    Ajout test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+# Exemple de secrets pour test Gitleaks
+
+# Clé AWS
+aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Clé API Google
+google_api_key = "AIzaSyD-FAKEKEY1234567890ABCDEFGHIJK"
+
+# Mot de passe codé en dur
+db_password = "MyP@ssword123"
+
+# Clé privée simulée
+-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKg...
+-----END PRIVATE KEY-----
+
